e151105beec670171633d4f211f977ca422a6315
José Dario Soto <sotomark2021@gmail.com>
2022-09-08 20:01:06 -0500

    Proyecto desde Cero de MinTic

---
+++ src/main/java/com/SotoWeb/ProyectoCiclo3/ProyectoCiclo3Application.java
@@ -0,0 +1,13 @@
+package com.SotoWeb.ProyectoCiclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCiclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCiclo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SotoWeb/ProyectoCiclo3/ProyectoCiclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SotoWeb.ProyectoCiclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCiclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
